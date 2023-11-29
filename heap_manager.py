12eaa6f84943e4ba26bd1ea0f4113454191f3c82
Imanm02 <imanm1381@gmail.com>
2023-11-29 19:05:09 +0330

    clean code

--- heap_manager.py
+++ heap_manager.py
@@ -7,10 +7,12 @@ class HeapManager:
 
         # Dictionary to store variables with their memory addresses as keys.
         self.variables = {}
-        
+
         # heap manager is called twice for arrays. firstly with the declare_id and secondly
         # with modify attributes. after the second call with have to modify array type for address of array.
         # last_assigned is used for this.
+        
+        # Used to track the last temporary variable created, especially for arrays.
         self.last_temp = None
 
     def get_temp(self, type_name, size=1, array_attribute=False):
